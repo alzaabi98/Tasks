a7f0f600b4087a8de625ffa5301c42b544e71ba6
alzaabi98@gmail.com <alzaabi98@gmail.com>
2016-09-07 21:23:31 +0400

    f
    c

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <h4> I am testing login via facebook</h4>
             </div>
         </div>
     </body>
